afa23c839e432825b070d601e421b979a3e027d3
udacithelp <52212693+udacithelp@users.noreply.github.com>
2019-06-25 13:11:38 -0500

    add kalman_filter copied from workspace

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -26,16 +26,61 @@ void KalmanFilter::Predict() {
   /**
    * TODO: predict the state
    */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
+  
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  //
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;  
+
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+  float rho = sqrt( x_(0) * x_(0) + x_(1) * x_(1));
+  VectorXd phi = VectorXd(3);
+  float rho_dot = ( x_(0) * x_(2) + x_(1) * x_(3) )/rho;
+  phi << rho, atan2(x_(1), x_(0)), rho_dot;
+  VectorXd y = z - phi;
+
+  //The angle should be between -pi and pi
+  while(phi < -M_PI) {
+    phi = phi + 2 * M_PI;
+  }
+  while(phi > M_PI)  {
+    phi = phi - 2 * M_PI;
+  }
+
+  y(1) = phi;
+  
+  //
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;      
 }
