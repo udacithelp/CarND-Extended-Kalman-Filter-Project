a10fa268209207495b40fb28a20ff34ef8d33088
udacithelp <52212693+udacithelp@users.noreply.github.com>
2019-06-26 11:17:44 -0500

    fix timestamp

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -120,6 +120,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   float noise_ay = 9;
 
   float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
+  previous_timestamp_ = measurement_pack.timestamp_;
   //
   float dt_2 = dt * dt;
   float dt_3 = dt_2 * dt;
